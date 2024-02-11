6c4851685f2b2c0b43f09c8ffec953290e1037f1
JuanFila <juanfilapablo@hotmail.com>
2024-02-11 14:16:50 -0300

    add hash password

--- src/main/java/br/com/juanteste/demo/user/IUserRepository.java
+++ src/main/java/br/com/juanteste/demo/user/IUserRepository.java
@@ -1,5 +1,10 @@
 package br.com.juanteste.demo.user;
 
-public interface IUserRepository {
-  
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel,  UUID> {
+  UserModel findByUsername(String username);
+
 }
